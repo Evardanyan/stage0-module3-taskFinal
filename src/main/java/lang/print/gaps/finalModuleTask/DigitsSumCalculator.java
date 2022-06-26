184e86a39af671a7d7720a88f9ee17bcd45883ec
Edgar Vardanyan <e.vardan4an@gmail.com>
2022-06-27 01:21:47 +0400

    first_try

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -1,6 +1,33 @@
 package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
-    public void calculateSum(int number) {
+//    674 % 10 = 4
+//
+//    Sum = 0 + 4 = 4
+//
+//            674 / 10 = 67
+//
+//    Second iteration
+//
+//67 % 10 = 7
+//
+//    Sum = 4 + 7 = 11
+//
+//            67 / 10 = 6
+
+    public static void calculateSum(int number) {
+        int sum = 0;
+        int first = number % 10;
+        sum = sum + first;
+        int second = number / 10;
+        int third = second % 10;
+        sum = sum + third;
+        int fourth = second / 10;
+        int fifth  = fourth % 10;
+        sum = sum + fifth;
+        int six = fourth / 10;
+        int seven = six % 10;
+        sum = sum + seven;
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -1,6 +1,15 @@
 package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
-    public void revert(int number) {
+    public static void revert(int number) {
+        int sum = 0;
+        int first = number % 10;
+        sum = sum + first;
+        int second = number / 10;
+        int third = second % 10;
+        sum = sum + third;
+        int fourth = second / 10;
+        int fifth  = fourth % 10;
+        System.out.println("" + first + third + fifth);
     }
 }
